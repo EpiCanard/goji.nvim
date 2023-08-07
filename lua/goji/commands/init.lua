8c8bb7dc6e853c821148b306706c9e140bb8a0fa
EpiCanard <epicanard@gmail.com>
2023-08-07 13:26:58 +0200

    feat(commands): add tab completion on command

--- lua/goji/commands/init.lua
+++ lua/goji/commands/init.lua
@@ -5,10 +5,33 @@ local builder = require("goji.request.query_builder")
 local log = require("goji.log")
 local M = {}
 
+local function filter_commmands(commands, search)
+  local filtered = {}
+
+  for _, cmd in pairs(commands) do
+    if string.sub(cmd, 1, #search) == search then
+      table.insert(filtered, cmd)
+    end
+  end
+
+  return filtered
+end
+
+local function tab_complete(arg, cmd_line)
+  local args = vim.split(vim.trim(cmd_line), " ")
+  local commands = vim.tbl_keys(M.commands)
+
+  if #args == 1 then
+    return commands
+  elseif #args == 2 then
+    return filter_commmands(commands, arg)
+  end
+end
+
 function M.setup()
   vim.api.nvim_create_user_command("Goji", function(opts)
     require("goji.commands").goji(unpack(opts.fargs))
-  end, { nargs = "*" })
+  end, { complete = tab_complete, nargs = "*" })
 
   M.commands = {
     issue = function(args)
